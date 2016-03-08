4f266aa8ca35696a08081c6ed765d6508bc6363f
Jesse Lucas <jesse@forestgiant.com>
2016-03-08 14:55:37 -0500

    Stubbing out npr command

---
+++ npr/npr.go
@@ -0,0 +1,2 @@
+// https://api.composer.nprstations.org/v1/widget/{ucs}/tracks?format=json&limit=14
+// https://api.composer.nprstations.org/v1/widget/517ed638e1c8d825471462ff/tracks?format=json&limit=14
