e224d513ca3ef0bc0f7535f6affe90ff35aae0be
Jesse Lucas <jesse@forestgiant.com>
2016-03-08 15:34:38 -0500

    Adding SLACK_API_QOTD key

--- commands/qotd/qotd.go
+++ commands/qotd/qotd.go
@@ -1,7 +1,9 @@
 package qotd
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
 	"github.com/jesselucas/slackcmd/slack"
 )
@@ -12,6 +14,15 @@ type Command struct {
 
 // Request is used to send back to slackcmd
 func (cmd *Command) Request(sc *slack.SlashCommand) (*slack.CommandPayload, error) {
+	// read credentials from environment variables
+	slackAPIKey := os.Getenv("SLACK_KEY_QOTD")
+
+	// Verify the request is coming from Slack
+	if sc.Token != slackAPIKey {
+		err := errors.New("Unauthorized Slack")
+		return nil, err
+	}
+
 	// create payload
 	cp := &slack.CommandPayload{
 		Channel:       fmt.Sprintf("@%v", sc.UserName),
